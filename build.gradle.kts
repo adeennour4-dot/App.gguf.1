95a7d41b72914de8215fb8f94d4b4d4b78ab894f
adeennour4-dot <adeennour4@gmail.com>
2026-06-13 02:39:24 +0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("com.android.application")            version "9.1.1"  apply false
-    // AGP 9.x has built-in Kotlin support; compose plugin is still required.
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
+    kotlin("android")                        version "2.1.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.21" apply false
 }
